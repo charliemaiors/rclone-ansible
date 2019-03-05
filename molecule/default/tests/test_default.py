f1bae73584a62ef7c0b1595a0bc4a2224e8b4c5a
charliemaiors <charliemaiors@gmail.com>
2019-03-05 18:11:57 +0100

    testing python

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,15 @@
 import os
-
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize('pkg', [
+  'rclone',
+])
+def test_pkg(host, pkg):
+    package = host.package(pkg)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert package.is_installed

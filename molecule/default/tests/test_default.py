1cadb7b16b04c4ea173361662da7e0f18e69dc69
charliemaiors <charliemaiors@gmail.com>
2019-03-06 14:34:07 +0100

    gitignore and tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,15 +1,14 @@
 import os
-import pytest
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.mark.parametrize('pkg', [
-  'rclone',
-])
-def test_pkg(host, pkg):
-    package = host.package(pkg)
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
 
-    assert package.is_installed
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
